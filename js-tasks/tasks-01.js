c48764f5fe44b467313639f65d042d4ed6abf654
Liliia Ishchenko <101570607+Liliia040002@users.noreply.github.com>
2023-09-17 14:14:05 +0300

    Create tasks-01.js

---
+++ js-tasks/tasks-01.js
@@ -0,0 +1,21 @@
+// Task 1. Messi Goals https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
+
+
+
+// Task 2. Make negative    https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
+
+
+
+//  Task 3. Game Move https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
+
+
+
+// Task 4. Personalized Message https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
+
+
+
+//  Task 5. Keep Hydrated https://www.codewars.com/kata/keep-hydrated-1/train/javascript
+
+
+
+// Task 6. Opposites Attract https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript

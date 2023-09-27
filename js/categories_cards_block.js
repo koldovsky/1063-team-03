b7de711de75d2fff3e3ac7f731f8ebbf30569517
Artur Ventlyand <113329020+ArturVentlyand@users.noreply.github.com>
2023-09-27 11:17:36 +0000

    Added api file, fixed categories.css, update categories_cards_block.js

--- js/categories_cards_block.js
+++ js/categories_cards_block.js
@@ -1,29 +1,5 @@
-const infoCards = [
-    {
-        id: 1,
-        name: ' Vintage Wines ',
-        description: 'Enjoy wines bottled in the 1980s and 1990s. Taste them and order delivery.',
-        image: 'img/img.categories.partial/illustration_info_wines.png',
-    },
-    {
-        id: 2,
-        name: ' The Best Grape ',
-        description: 'We use only the highest quality grapes from our plantations in Texas.',
-        image: 'img/img.categories.partial/illustration_info_grape.png',
-    },
-    {
-        id: 3,
-        name: ' Beautiful Bottles ',
-        description: 'We work with designers to create the most beautiful wine bottles.',
-        image: 'img/img.categories.partial/illustration_info_bottles.png',
-    },
-    {
-        id: 4,
-        name: ' Interesting Tours ',
-        description: 'Sign up for a tour and tasting and learn more about our wines.',
-        image: 'img/img.categories.partial/illustration_info_tours.png',
-    },
-]
+const response = await fetch('api/info-cards.json');
+const infoCards = await response.json();
 
 renderCards(infoCards);
 
@@ -34,8 +10,8 @@ function renderCards(cards) {
         cardsContainer.innerHTML += 
             `<div class="cards-block">
                 <img class="info-img" src="${card.image}" alt="${card.name}">
-                <h3 class="main-text sub-fonts">${card.name}</h3>
-                <p class="sub-text">${card.description}</p>
+                <h4 class="info-title main-text sub-fonts">${card.name}</h4>
+                <p class="info-description sub-text">${card.description}</p>
                 <span class="highlighted-item-brand-color">
                     <a class="button" href="about-us.html"> Learn More </a>
                 </span>

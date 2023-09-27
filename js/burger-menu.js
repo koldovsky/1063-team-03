e0606632aa07165f92f90c2781204343ba0d3f8a
Vova-Romaniuk <romanyuk1504@gmail.com>
2023-09-27 16:12:40 +0300

    fixed import burger-menu.js

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -1,25 +1,23 @@
-window.setTimeout(
-	() => {
-		const toogleMenu = document.querySelector(".toggle-menu");
-		const containerMenu = document.querySelector(".container-menu");
-		
-		toogleMenu.addEventListener("click", function (e) {
-			toogleMenu.classList.toggle("toggle-active");
-			containerMenu.classList.toggle("container-menu-active");
-			document.body.classList.toggle("lock");
-		});
 
-		const navLinks = document.querySelectorAll(".header__menu-item");
+const toogleMenu = document.querySelector(".toggle-menu");
+const containerMenu = document.querySelector(".container-menu");
 
-		navLinks.forEach((navLink) => {
-			navLink.addEventListener("click", (_) => {
-					if (toogleMenu.classList.contains("active")) {
-						toogleMenu.classList.remove("toggle-active");
-						containerMenu.classList.remove("container-menu-active");
-						document.body.classList.remove("lock");
-					}
-				}
-			);
-		});
-	}
-,500);
+toogleMenu.addEventListener("click", function (e) {
+	toogleMenu.classList.toggle("toggle-active");
+	containerMenu.classList.toggle("container-menu-active");
+	document.body.classList.toggle("lock");
+});
+
+const navLinks = document.querySelectorAll(".header__menu-item");
+
+navLinks.forEach((navLink) => {
+	navLink.addEventListener("click", (_) => {
+			if (toogleMenu.classList.contains("active")) {
+				toogleMenu.classList.remove("toggle-active");
+				containerMenu.classList.remove("container-menu-active");
+				document.body.classList.remove("lock");
+			}
+		}
+	);
+});
+
